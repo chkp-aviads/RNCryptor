f70e23ca38e0baae1262fdf833cfa42084bf836b
Aviad Segev <aviads@checkpoint.com>
2024-09-30 10:57:01 +0300

    Added dynamic SPM library option

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .library(
             name: "RNCryptor",
             targets: ["RNCryptor"]),
+        .library(name: "RNCryptorDynamic", type: .dynamic, targets: ["RNCryptor"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
